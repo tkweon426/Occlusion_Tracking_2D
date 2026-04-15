9322e65fb163c3d7dc08b9ff157e2e8e90f916c5
tkweon426 <thomas426789@gmail.com>
2026-04-15 00:33:55 -0400

    update single circle obstacle world

--- environments/single_circle.py
+++ environments/single_circle.py
@@ -1,8 +1,7 @@
 # environments/single_circle.py
 from environments.base_env import BaseEnvironment, CircleObstacle
 
-
 def make_single_circle_env():
     return BaseEnvironment(obstacles=[
-        CircleObstacle(cx=5.0, cy=5.0, radius=3.0)
+        CircleObstacle(cx=3.0, cy=8.0, radius=1.0)
     ])
